d47b5e5f8af122e2f7b97b3c55020130a8eedf4f
JohanHorsmans <54767010+JohanHorsmans@users.noreply.github.com>
2021-05-22 15:57:52 +0200

    Create create_lang_venv.sh

---
+++ create_lang_venv.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+VENVNAME=lang_venv
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+pip install ipython
+pip install jupyter
+
+# Loading these here, since they are giving trouble in the requirements.txt
+pip install matplotlib
+pip install opencv-python
+pip install pydot
+pip install pydotplus
+pip install graphviz
+
+python -m ipykernel install --user --name=$VENVNAME
+
+test -f requirements.txt && pip install -r requirements.txt
+
+deactivate
+echo "build $VENVNAME"
