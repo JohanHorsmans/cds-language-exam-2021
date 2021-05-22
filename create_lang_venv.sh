abdcb16e832fd36d5a6fc5090ddf70b383623b9e
JohanHorsmans <54767010+JohanHorsmans@users.noreply.github.com>
2021-05-22 18:12:05 +0200

    Update create_lang_venv.sh

--- create_lang_venv.sh
+++ create_lang_venv.sh
@@ -9,13 +9,6 @@ pip install --upgrade pip
 pip install ipython
 pip install jupyter
 
-# Loading these here, since they are giving trouble in the requirements.txt
-pip install matplotlib
-pip install opencv-python
-pip install pydot
-pip install pydotplus
-pip install graphviz
-
 python -m ipykernel install --user --name=$VENVNAME
 
 test -f requirements.txt && pip install -r requirements.txt
