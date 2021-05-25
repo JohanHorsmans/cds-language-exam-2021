5a22cef6155e122bd5c67fdbcd3dc70858664414
JohanHorsmans <54767010+JohanHorsmans@users.noreply.github.com>
2021-05-25 22:00:21 +0200

    Updated assignment 5

--- assignment_5/HateClass.py
+++ assignment_5/HateClass.py
@@ -1,16 +1,16 @@
-# system tools
+# System tools:
 import os
 import sys
 sys.path.append(os.path.join(".."))
 
-# data munging tools
+# Data munging tools:
 import pandas as pd
 import numpy as np
 import utils.classifier_utils as clf
 from utils.neuralnetwork import NeuralNetwork
 
 
-# Machine learning stuff
+# Machine learning stuff:
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import ShuffleSplit
@@ -20,105 +20,120 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn import tree, svm
 from sklearn.neural_network import MLPClassifier
 
+# Set seed for reproducibility:
+import random
+random.seed(10)
 
+# Define the main function of the script: 
 def main():
     
-    #Create filenames to load train and test data
-    filename = os.path.join("data","Train_Hate.tsv")
-    filename_test = os.path.join("data", "Test_Hate.tsv")
+    print("[INFO]: Loading and preprocessing data")
+    # Specify filepaths to load training- and testing data:
+    filepath = os.path.join("data","Train_Hate.tsv")
+    filepath_test = os.path.join("data", "Test_Hate.tsv")
 
-    #load Data    
-    DATA = pd.read_csv(filename, index_col=0, sep="\t")
-    DATA_TEST = pd.read_csv(filename_test, index_col=0, sep="\t")
+    # Load Data:
+    Data = pd.read_csv(filepath, index_col=0, sep="\t")
+    Data_test = pd.read_csv(filepath_test, index_col=0, sep="\t")
     
-    #Preprocessing: Dropping NA's 
-    DATA = DATA.dropna()
-    DATA_TEST = DATA_TEST.dropna()
+    # Preprocessing: Dropping NA's:
+    Data = Data.dropna()
+    Data_test = Data_test.dropna()
 
-    #Converting label-column to 0's and 1's
-    DATA["subtask_a"] = np.where(DATA["subtask_a"] == 'NOT', 0, 1)
-    DATA_TEST["subtask_a"] = np.where(DATA_TEST["subtask_a"] == 'NOT', 0, 1)
+    # Converting label-column from "NOT" an "OFF" to 0's and 1's, respectively: 
+    Data["subtask_a"] = np.where(Data["subtask_a"] == 'NOT', 0, 1)
+    Data_test["subtask_a"] = np.where(Data_test["subtask_a"] == 'NOT', 0, 1)
     
-    #Dividing data into X and y variables
-    X_train = DATA["tweet"]
-    y_train = DATA["subtask_a"]
-    X_test = DATA_TEST["tweet"]
-    y_test = DATA_TEST["subtask_a"]
+    # Dividing data into X and y variables:
+    X_train = Data["tweet"]
+    y_train = Data["subtask_a"]
+    X_test = Data_test["tweet"]
+    y_test = Data_test["subtask_a"]
     
-    #Vectorizing to make unigrams and bigrams and remove:
-    #-frequently used words
-    
+    # Vectorizing to make lowercased unigrams and bigrams with tf-idf transformation and remove 
+    #-frequently used words:
     vectorizer = TfidfVectorizer(ngram_range = (1,2),
                              lowercase =  True,
                              max_df = 0.95)
 
-    #Vectorize Training data
+    # Vectorize training data:
     X_train_feats = vectorizer.fit_transform(X_train)
-    #Vectorize Test data
+    # Vectorize testing data:
     X_test_feats = vectorizer.transform(X_test)
-    #List feature names
+    # List feature names:
     feature_names = vectorizer.get_feature_names()
     
     
-    #Classifiers
+    print("[INFO]: Building, training and testing models")
+    # Build classifiers:
+    
+    # LOGISTIC REGRESSION:
+    
+    # Fit Logistic Regression model:
     lr_classifier = LogisticRegression(random_state=42).fit(X_train_feats, y_train)
 
-    #Logistic Regression predictions
+    # Get Logistic Regression predictions:
     lr_predictions = lr_classifier.predict(X_test_feats)
 
-    # Instantiate model with 1000 decision trees
+    # RANDOM FOREST:
+    
+    # Instantiate random forest model with 1000 decision trees:
     rf = RandomForestRegressor(n_estimators = 100, random_state = 42)
-    #Train the model on training data
+    
+    # Train the model on training data:
     rf.fit(X_train_feats, y_train);
 
-    #Random Forest predictions
+    # Get Random Forest predictions:
     rf_predictions = rf.predict(X_test_feats)
+    
+    # SVM:
 
-
-    #Initialize SVM classifier
+    # Initialize SVM classifier:
     clf_svm = svm.SVC(kernel='linear')
     
-    #Fit SVM
+    # Fit SVM:
     clf_svm = clf_svm.fit(X_train_feats, y_train)
     
-    #Get SVM predictions
+    # Get SVM predictions:
     svm_predictions = clf_svm.predict(X_test_feats)
 
 
-    #Decicision tree
+    # DECISION TREE:
+    
+    # Initialize model:
     clf_dt = tree.DecisionTreeClassifier()
     
-    #Fit Decision tree
+    # Fit Decision tree:
     clf_dt = clf_dt.fit(X_train_feats, y_train)
 
-    #Get Decision tree predictions
+    # Get Decision tree predictions
     dt_predictions = clf_dt.predict(X_test_feats)
 
+    # NEURAL NETWORK:
 
-    #Neural Network
+    # Initialize Neural Network:
     clf_nn = MLPClassifier(solver='lbfgs', alpha=1e-5,
                          hidden_layer_sizes=(256, 128), random_state=1)
 
 
-    #Fitting Neural network
+    # Fitting Neural network:
     clf_nn = clf_nn.fit(X_train_feats, y_train)
 
-    #Getting neural network predictions
+    # Get neural network predictions:
     nn_predictions = clf_nn.predict(X_test_feats)
-    
 
-    #Creating ensemble predictions by adding them together and setting a threshold
+    print("[INFO]: Ensembling")
+    
+    # Creating ensemble predictions by adding all model-predictions together and setting a threshold of 1.4 (i.e. if accumulated probability of a comment being offensive exceeds 1.4, classify it as offensive. Otherwise, classify it as non-offensive:
     preds = np.where(rf_predictions+nn_predictions+dt_predictions+lr_predictions > 1.4, 1, 0)
 
-    print("Confusion Matrix:")
+    print("[INFO]: Confusion Matrix:")
     print(pd.crosstab(y_test, preds))
 
-    print("Ckassification report:")
+    print("[INFO]: Classification report:")
     print(classification_report(y_test, preds))
     
-    print("A macro F1-score of .71 would place us in a 23rd place in the competition. Not bad for a computationally inexpensive ensemble model.")
+    print("A macro F1-score of .71 would place us in a 23rd place (out of 38) in the OffensEval2020-competition. Not bad for a computationally inexpensive ensemble model.")
     
 if __name__=="__main__":
     main()
-
-    
