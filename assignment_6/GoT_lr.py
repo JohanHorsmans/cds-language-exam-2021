14ce06e04ec965bd33499dda3a72d64874d9988e
JohanHorsmans <54767010+JohanHorsmans@users.noreply.github.com>
2021-05-25 22:08:39 +0200

    Updated assignment 6

--- assignment_6/GoT_lr.py
+++ assignment_6/GoT_lr.py
@@ -22,6 +22,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import ShuffleSplit
 from sklearn import metrics
 
+# Set seed for reproducibility:
+import random
+random.seed(10)
+
 # Define the main function of the script:
 def main():
     
